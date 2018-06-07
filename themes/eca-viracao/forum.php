1a97049aa9003c031c5a05cf7f0552d349cdfba4
viniciusofp <viniciusofp@gmail.com>
2018-06-07 13:11:17 -0300

    last updates

---
+++ themes/eca-viracao/forum.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<?php if (have_posts()): while(have_posts()) : the_post(); ?>
+	<div class="single-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="single-content">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 col-md-10 the-post">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endwhile; endif ?>
+
+<?php get_footer() ?>
