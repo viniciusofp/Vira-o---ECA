10806e23162c9f4bcddf474f677cc147f92e5aa8
viniciusofp <viniciusofp@gmail.com>
2018-06-08 15:23:24 -0300

    page.php

--- themes/eca-viracao/page.php
+++ themes/eca-viracao/page.php
@@ -1,40 +1,24 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package ECA_-_Viração
- */
+<?php get_header(); ?>
+<?php if (have_posts()): while(have_posts()) : the_post(); ?>
+	<div class="single-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="single-content">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 the-post">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endwhile; endif ?>
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_sidebar() ?>
+<?php get_footer() ?>
