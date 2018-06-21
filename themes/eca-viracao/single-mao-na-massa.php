7d0225ebe05f4cb00051cba83597c7c183f13571
viniciusofp <viniciusofp@gmail.com>
2018-06-21 10:24:40 -0300

    feedback / correcoes

--- themes/eca-viracao/single-mao-na-massa.php
+++ themes/eca-viracao/single-mao-na-massa.php
@@ -9,13 +9,40 @@
 			</div>
 		</div>
 	</div>
+	<div class="conceitos-menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<ul class="list-inline">
+						<?php 
+							$args = array(
+								'post_type' => 'conceitos',
+							  'posts_per_page' => -1,
+							);
+
+							$the_query = new WP_Query( $args ); 
+
+							if ( $the_query->have_posts() ) : ?>
+								<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+								<li class="list-inline-item">
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</li>
+								<?php endwhile; ?>
+								<?php wp_reset_postdata(); ?>
+							<?php endif; ?>
+					</ul>
+				</div>			
+			</div>
+	
+		</div>
+	</div>
 	<div class="single-content">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-12 col-md-10 the-post">
 					<a href="#" onclick="window.history.back()"><i class="fa fa-arrow-left mr-2"></i>Voltar</a>
 					<div class="divisor"></div>
-					<h1 class="big"><?php the_title(); ?></h1>
+					<h1 class="big d-none"><?php the_title(); ?></h1>
 					<div class="mb-1">
 
 					</div>
@@ -28,4 +55,13 @@
 	
 	
 <?php endwhile; endif ?>
+<script>
+	var links = jQuery('.conceitos-menu li a');
+	for (var i = links.length - 1; i >= 0; i--) {
+		var linkTitle = jQuery(links[i]).html();
+		if (linkTitle == jQuery('h1.big').html() ) {
+			jQuery(links[i]).addClass('active')
+		}
+	}
+</script>
 <?php get_footer() ?>
--- themes/eca-viracao/single-quiz.php
+++ themes/eca-viracao/single-quiz.php
@@ -9,14 +9,14 @@
 			</div>
 		</div>
 	</div>
-<!-- 	<div class="conceitos-menu">
+	<div class="conceitos-menu">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
 					<ul class="list-inline">
 						<?php 
 							$args = array(
-								'post_type' => 'quiz',
+								'post_type' => 'conceitos',
 							  'posts_per_page' => -1,
 							);
 
@@ -25,9 +25,7 @@
 							if ( $the_query->have_posts() ) : ?>
 								<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 								<li class="list-inline-item">
-									<a href="<?php the_permalink(); ?>">
-										<?php the_title(); ?>
-									</a>
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 								</li>
 								<?php endwhile; ?>
 								<?php wp_reset_postdata(); ?>
@@ -37,14 +35,14 @@
 			</div>
 	
 		</div>
-	</div> -->
+	</div>
 	<div class="single-content">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-12 col-md-10 col-lg-6 the-post">
 					<a href="#" onclick="window.history.back()"><i class="fa fa-arrow-left mr-2"></i>Voltar</a>
 					<div class="divisor"></div>
-					<h1 class="big"><?php the_title(); ?></h1>
+					<h1 class="big d-none"><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 					<?php $loop = 1; if( have_rows('pergunta') ): while ( have_rows('pergunta') ) : the_row();
 					?>
@@ -77,8 +75,8 @@
 
 	var links = jQuery('.conceitos-menu li a');
 	for (var i = links.length - 1; i >= 0; i--) {
-		var linkHref = jQuery(links[i]).attr('href');
-		if (linkHref == window.location.href ) {
+		var linkTitle = jQuery(links[i]).html();
+		if (linkTitle == jQuery('h1.big').html() ) {
 			jQuery(links[i]).addClass('active')
 		}
 	}
